c8f53ce9804315d4756c2c3f80da5e0ee56bd1d8
Achmad Fauzan Nur <achmadfa3@gmail.com>
2021-04-19 08:46:32 +0700

    job 10

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  export default {
+    apiKey: "AIzaSyB0cNrZrMe42fBYk1lzUtlLZKbn3RSAAdk",
+    authDomain: "fir-a28ec.firebaseapp.com",
+    projectId: "fir-a28ec",
+    storageBucket: "fir-a28ec.appspot.com",
+    messagingSenderId: "541586929186",
+    appId: "1:541586929186:web:215aa58a2fcc95f546bc7b",
+    measurementId: "G-L2X5X8PWET"
+  };
